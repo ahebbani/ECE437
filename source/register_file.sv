781f4ba5a7afda09b9b1f2edccd09c7eec6e85fb
Aditya Hebbani <ahebbani@purdue.edu>
2025-08-26 13:58:05 -0400

    register done?

---
+++ source/register_file.sv
@@ -0,0 +1,35 @@
+`include "register_file_if.vh"
+`include "cpu_types_pkg.vh"
+
+module register_file (
+    input logic clk, nrst,
+    register_file_if.rf rfif
+);
+
+word_t [31:0] register, next_register;
+
+always_comb begin : write
+    if (rfif.wen) begin
+        if (rfif.wsel != 0) begin
+            next_register[rfif.wsel] = rfif.wdat;
+        end
+    end
+end
+
+always_comb begin: read
+    rfif.rdat1 = register[rfif.rsel1];
+    rfif.rdat2 = register[rfif.rsel2];
+end
+
+always_ff @( posedge clk, negedge nrst ) begin
+    if (!nrst) begin
+        register <= '0;
+    end
+    else begin
+        register <= next_register;
+    end
+end
+
+
+
+endmodule
