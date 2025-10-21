68d659b5581422a58c8d386ab6f22e993ef2dbce
Aditya Hebbani <ahebbani@purdue.edu>
2025-10-21 13:03:34 -0400

    ldst case

---
+++ asmFiles/ldst2.asm
@@ -0,0 +1,16 @@
+li t0, 1
+li t2, 2
+li x17, 0
+li sp, 0xFFFC
+li x16, 5
+sw x16, 0($31)
+lw x16, 0(x4)
+sw x16, 0($31)
+lw x16, 0(x4)
+sw x16, 0($31)
+lw x16, 0(x4)
+sw x16, 0($31)
+lw x16, 0(x4)
+halt
+sw x16, 0($31)
+
